5d41a050852628ef792e23beade6f8cdeb971a76
nils <nilsriebeling@yahoo.de>
2019-11-01 10:49:15 +0800

    Initial Commit

---
+++ On The Map/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  On The Map
+//
+//  Created by Nils Riebeling on 01.11.19.
+//  Copyright © 2019 Nils Riebeling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
